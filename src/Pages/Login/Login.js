bcdacd6ce7b4fb68c4300ff71c59f2302dc475be
SVAYounii <younes048@gmail.com>
2023-01-19 21:54:42 +0100

    login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -66,7 +66,7 @@ export function GetAllUsers() {
   console.log(data);
   var config = {
     method: 'post',
-    url: ConnectionString() + '/api/Users/Login',
+    url: ConnectionString() + '/api/Users/LoginUser',
     headers: {
       'Content-Type': 'application/json'
     },
